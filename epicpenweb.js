9c7541bcb25d1d80bd49e8338f34bacb59c13b37
JH.ZEHAN CODER <zehanjh93@gmail.com>
2024-09-05 00:23:00 +0600

    Add files via upload

---
+++ epicpenweb.js
@@ -0,0 +1,76 @@
+let navber = document.querySelector(".liItem");
+
+let menu = document.querySelector(".icon");
+
+menu.addEventListener("click", () => {
+    navber.classList.toggle("block");
+    navber.classList.add("ri-close-large-fill");
+
+    let ndnav = document.querySelector(".ri-close-large-fill");
+
+    ndnav.style.fontSize = "1.6rem";
+    ndnav.style.cursor = "pointer";
+    ndnav.style.margin = "1vw 15vw";
+    
+
+    ndnav.addEventListener("click", () => {
+        navber.classList.add("block");
+    })
+
+});
+
+
+
+let playIcon = document.querySelector(".jh");
+
+let noneImg = document.querySelector(".singleLeft");
+
+let video = document.querySelector(".singleLeft2")
+
+
+
+playIcon.addEventListener("click", () => {
+    playIcon.classList.toggle("ri-play-circle-line");
+    playIcon.classList.add("ri-pause-circle-line");
+
+    noneImg.classList.toggle("singleLeft3");
+    video.style.display = "block";
+    
+
+    
+});
+
+
+let cancel = document.querySelector(".cancel>i");
+
+cancel.addEventListener("click", () => {
+    video.style.display = "none";
+    noneImg.classList.remove("singleLeft3");
+    playIcon.classList.add("ri-play-circle-line");
+    
+});
+
+
+
+// document.addEventListener("scroll", () => {
+//     let header = document.querySelector(".navItem");
+
+//     if(window.scrollY > 0) {
+//         header.classList.add("scrollColor");
+//     }else{
+//         header.classList.remove("scrollColor");
+//     }
+// });
+
+
+
+document.addEventListener("scroll", () => {
+    let header = document.querySelector(".navItem");
+
+    if(window.scrollY > 0) {
+        header.classList.add("scrollColor");
+
+    }else{
+        header.classList.remove("scrollColor");
+    }
+})
